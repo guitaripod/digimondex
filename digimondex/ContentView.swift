6dbe599314ee987d9f4edda5f31284f599b423af
Marcus Ziadé <marcus.ziade@ouraring.com>
2024-01-14 12:06:44 -0800

    Initial Commit

---
+++ digimondex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  digimondex
+//
+//  Created by Marcus Ziadé on 14.1.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ digimondex/digimondexApp.swift
@@ -0,0 +1,17 @@
+//
+//  digimondexApp.swift
+//  digimondex
+//
+//  Created by Marcus Ziadé on 14.1.2024.
+//
+
+import SwiftUI
+
+@main
+struct digimondexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
